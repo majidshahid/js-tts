3792c8281196029a4f05ce237f6e1a75c93ffe7d
majid shahid <majidshahid474@gmail.com>
2021-05-20 14:22:34 -0700

    js tts

---
+++ app.js
@@ -0,0 +1,29 @@
+// var firstName= prompt("Enter your first name","Majid");
+// var lastName=prompt("Enter your last name","Shahid");
+
+// var fullName = firstName + " "+lastName;
+// document.write(fullName + "<br>")
+
+
+// var  mobile=prompt("Enter your favorite mobile phone modle","iphone x " );
+//  document.write("your favorite phone : " +mobile + " "+"<br>");
+//  document.write("lenght of string : " + mobile.length)
+
+
+var a = "pakistani";
+document.write("string : " + a + "<br>");
+document.write( "index of 'n' : "+a.indexOf("n") +"<br>"+"<br>"+"<br>")
+
+
+
+var a = "hello world";
+document.write("string : " + a + "<br>");
+document.write( "lastIndex of 'l' : "+a.lastIndexOf("l" ) + "<br>"+"<br>"+"<br>")
+
+
+
+
+
+var a = "pakistani";
+document.write("string : " + a + "<br>");
+document.write( "character at index  3 : "+a.charAt(3) +"<br>"+"<br>"+"<br>")
